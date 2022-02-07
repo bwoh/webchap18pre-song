6f3c1f23f817e13cd90e0c4c121dd22556a72730
BWOh <bwohkit@gmail.com>
2022-02-08 04:00:08 +0900

    🚀 finish useState

--- src/SongList.js
+++ src/SongList.js
@@ -2,11 +2,9 @@ import React, { useState } from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
-import Grid from "@mui/material/Grid";
-import Typography from "@mui/material/Typography";
-import FolderIcon from "@mui/icons-material/Folder";
+import MusicIcon from "@mui/icons-material/MusicVideo";
 import DeleteIcon from "@mui/icons-material/Delete";
-import { styled } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemAvatar from "@mui/material/ListItemAvatar";
@@ -14,82 +12,83 @@ import Avatar from "@mui/material/Avatar";
 import IconButton from "@mui/material/IconButton";
 import ListItemText from "@mui/material/ListItemText";
 
-function generate(element) {
-  return [0, 1, 2].map((value) =>
-    React.cloneElement(element, {
-      key: value
-    })
-  );
-}
-
-const Demo = styled("div")(({ theme }) => ({
-  backgroundColor: theme.palette.background.paper
-}));
-
 const SongList = () => {
-  const [dense, setDense] = React.useState(false);
-  const [secondary, setSecondary] = React.useState(false);
+  const [songName, setSongName] = useState("");
+  const [songs, setSongs] = useState([]);
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    // eslint-disable-next-line no-console
-    console.log({
-      email: data.get("email")
-    });
+  const addSong = (e) => {
+    e.preventDefault();
+    console.log(songName);
+    setSongs([...songs, songName]);
+    setSongName("");
+  };
+
+  const SongListItem = ({ idx, title }) => {
+    return (
+      <Paper>
+        <ListItem
+          button
+          component="a"
+          href={`https://www.youtube.com/results?search_query=노래방+${title}`}
+          target="_black"
+          sx={{ mb: 1, backgroundColor: "beige" }}
+          secondaryAction={
+            <IconButton
+              edge="end"
+              onClick={(e) => {
+                const deleted = [...songs];
+                deleted.splice(idx, 1);
+                setSongs(deleted);
+                // 원래 ListItem 실행 방지
+                e.preventDefault();
+              }}
+            >
+              <DeleteIcon />
+            </IconButton>
+          }
+        >
+          <ListItemAvatar>
+            <Avatar>
+              <MusicIcon />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText
+            primary={title}
+            secondary={`https://www.youtube.com/results?search_query=노래방+${title}`}
+          />
+        </ListItem>
+      </Paper>
+    );
   };
 
   return (
     <>
-      <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+      <Box component="form" onSubmit={addSong} sx={{ mt: 3 }}>
         <TextField
-          margin="form"
           required
+          autoFocus
           fullWidth
-          sx={{ mt: 2 }}
           id="songInput"
           label="노래 제목"
           name="songInput"
-          autoFocus
+          value={songName}
+          onChange={(e) => setSongName(e.currentTarget.value)}
         />
         <Button
           type="submit"
           fullWidth
           variant="contained"
-          sx={{ mt: 3, mb: 2 }}
+          sx={{ mt: 2, mb: 3 }}
         >
           노래 추가
         </Button>
       </Box>
 
-      <Grid item xs={12} md={6}>
-        <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
-          Avatar with text and icon
-        </Typography>
-        <Demo>
-          <List dense={dense}>
-            {generate(
-              <ListItem
-                secondaryAction={
-                  <IconButton edge="end" aria-label="delete">
-                    <DeleteIcon />
-                  </IconButton>
-                }
-              >
-                <ListItemAvatar>
-                  <Avatar>
-                    <FolderIcon />
-                  </Avatar>
-                </ListItemAvatar>
-                <ListItemText
-                  primary="Single-line item"
-                  secondary={secondary ? "Secondary text" : null}
-                />
-              </ListItem>
-            )}
-          </List>
-        </Demo>
-      </Grid>
+      <List>
+        {songs.map((song, index) => (
+          <SongListItem key={index} idx={index} title={song} />
+        ))}
+      </List>
     </>
   );
 };
